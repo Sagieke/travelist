966abdce2827317a6ac7195bb81286b25a783745
guykolbck <93705741+guykolbck@users.noreply.github.com>
2021-11-29 15:47:28 +0200

    Remove List button
    update-remove list buttonn

---
+++ react-frontend/src/components/RemoveList.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {Container,Row, Col,Button,ListGroup} from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MainPage from "./MainPage";
+
+export default function RemoveList(){
+return(
+ 
+ <div>
+ <Button renderAs="button">
+   remove list
+  </Button>
+ 
+</div>   
+ )
+
+
+}
