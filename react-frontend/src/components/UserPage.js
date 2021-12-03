473241fd2e841f4847e624d2c5c7af2c75dea21d
Sagieke <90636945+Sagieke@users.noreply.github.com>
2021-12-03 14:24:48 +0200

    connect back of addlist to front

--- react-frontend/src/components/UserPage.js
+++ react-frontend/src/components/UserPage.js
@@ -1,11 +1,29 @@
 import AddList from "./addlist";
-import React from "react";
+import React , { useState, useEffect }from "react";
 import {Row,ListGroup} from "react-bootstrap";
 
-import  GetRequestHooks from "./react-hook-get";
-import PostRequestHooks from "./react-hook-post";
-
 export default function  UserPage()  {
+  const [listgiInfo, setlistgiInfo] = useState([]);
+  useEffect(() => {
+      fetch('http://localhost:5000/getlists',{
+          credentials: "include"
+        })
+          .then(response => response.json())
+          .then(object => setlistgiInfo(object))
+      },[]);
+const ListGroupItem = (lgi, index) => {
+  return (
+
+<ListGroup.Item  variant="default"    key={index} style={{ textAlign: 'right', color: "white", background: lgi.color }} as="li" action href="#link1" >
+{lgi.name}            
+</ListGroup.Item>
+
+
+  )
+
+   
+};
+
 
 
 
@@ -24,7 +42,7 @@ export default function  UserPage()  {
 
           <ListGroup defaultActiveKey="#link1" as="ol" numbered>
             
-
+          {listgiInfo.map(ListGroupItem)}
         </ListGroup>
           </Row>
           <br />
@@ -36,7 +54,7 @@ export default function  UserPage()  {
           <div className="auth-inner-right">
           <h1>most search places</h1>
           <hr class="my-4"></hr>
-          <GetRequestHooks/>
+        
           </div>
           </Row>
         </div>
