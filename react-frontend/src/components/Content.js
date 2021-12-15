d3dbc5f7dbc6b0b5fbf6763058881a53610702d7
guykolbck <93705741+guykolbck@users.noreply.github.com>
2021-12-15 11:13:03 +0200

    Admin page frontend

---
+++ react-frontend/src/components/Content.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+export default class Content extends Component {
+    render(){
+        return (
+            <div className="content-wrapper">
+                <section className="content-header">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="box">
+                                <div className="box-header with-border">
+                                    <h3 className="box-title">Monthly Recap Report</h3>
+                                </div>
+                                <div className="box-body">
+                                    <div className="row">
+                                        <div className="col-md-8">
+                                            <p className="text-center">
+                                                <strong>Fuck u dani</strong>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="box-footer">
+                                    <div className="row">
+                                        <div className="col-sm-3 col-xs-6">
+                                            <div className="description-block border-right">
+                                                <span className="description-percentage text-green"><i className="fa fa-caret-up"></i> 17%</span>
+                                            </div>
+                                        </div>    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
