b88e75d3cc6e342f6e130403dce84563656efe18
zebigdt <dannytalor96@gmail.com>
2021-12-15 10:39:01 +0200

    added listofplaces table

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -9,6 +9,7 @@ class User(db.Model): #user data base
 
 @dataclass
 class ListOfLists(db.Model):
+    __tablename__ = 'listoflists'
     user_id: int
     id: int
     name: str
@@ -18,5 +19,23 @@ class ListOfLists(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     color = db.Column(db.String(7), nullable = False)
+
+@dataclass
+class ListOfPlaces(db.Model):
+    user_id: int
+    list_id: int
+    list_name: str
+    id: int
+    name: str
+    start_date: str
+    end_date: str
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    list_id = db.Column(db.Integer, db.ForeignKey('listoflists.id'))
+    list_name = db.Column(db.String(100), db.ForeignKey('listoflists.name'))
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    start_date = db.Column(db.String(8), nullable = False)
+    end_date = db.Column(db.String(8), nullable = False)
 
 db.create_all()
