528ee11fdb4c8236a6f901220ec6a398e47ffa84
zebigdt <dannytalor96@gmail.com>
2021-12-15 16:04:38 +0200

    viewing, adding, removing places in list functionality added

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -72,19 +72,49 @@ def removelist():
     if request.method == 'POST':
         user_id = session.get("user_id")
         id = request.form['id']
-        lists = ListOfLists.query.filter_by(user_id = user_id,id=id).first()
-        db.session.delete(lists)
+        list = ListOfLists.query.filter_by(user_id = user_id,id=id).first()
+        db.session.delete(list)
         db.session.commit()
         return redirect('http://localhost:3000/userPage')
+
+@app.route('/addplace', methods=['GET','POST'])
+def addplace():
+    if request.method == 'POST':
+        user_id = session.get("user_id")
+        list_id = session.get("list_id")
+        place_name = request.form['PlaceName']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        new_place = ListOfPlaces(user_id = user_id, list_id = list_id, name = place_name,start_date=start_date,end_date=end_date)
+        db.session.add(new_place)
+        db.session.commit()
+        return redirect('http://localhost:3000/userpage/places')
+
+@app.route('/removeplace', methods=['GET','POST'])
+def removeplace():
+    if request.method == 'POST':
+        user_id = session.get('user_id')
+        list_id = session.get('list_id')
+        id = request.form['id']
+        place = ListOfPlaces.query.filter_by(user_id=user_id,list_id=list_id,id=id).first()
+        db.session.delete(place)
+        db.session.commit()
+        return redirect('http://localhost:3000/userPage/places')
 
 @app.route('/getplaces', methods=['GET', 'POST'])
 def getplaces():
     if request.method == 'GET':
         user_id = session.get('user_id')
-        list_id = request.form['list_id']
-        print("USER ID: {}".format(user_id))
+        list_id = session.get('list_id')
         places = ListOfPlaces.query.filter_by(user_id=user_id,list_id=list_id).all()
         return jsonify(places)
+
+@app.route('/viewlist', methods=['GET', 'POST'])
+def viewlist():
+    if request.method == 'POST':
+        list_id = request.form['list_id']
+        session['list_id'] = list_id
+    return redirect('http://localhost:3000/UserPage/places')
 
 if __name__ == '__main__':
     app.run()
