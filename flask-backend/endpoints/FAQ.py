5bf9f8f92867e455e53ed42f0751b4a57e5cd9b3
MarkOS5623 <markos5623@gmail.com>
2021-12-24 16:10:46 +0200

    added update FAQ functionality

--- flask-backend/endpoints/FAQ.py
+++ flask-backend/endpoints/FAQ.py
@@ -3,7 +3,7 @@ from app import db
 from models import FAQ
 FAQ = Blueprint('FAQ',__name__)
 
-@FAQ.route('\addFAQ', methods=['GET', 'POST'])
+@FAQ.route('/addFAQ', methods=['GET', 'POST'])
 def addFAQ():
     if request.method == 'POST':
         question = request.form('question')
@@ -13,11 +13,22 @@ def addFAQ():
         db.session.commit() 
         return redirect('http://localhost:3000/faqUpdate')
 
-@FAQ.route('\deleteFAQ', methods=['GET', 'POST'])
+@FAQ.route('/deleteFAQ', methods=['GET', 'POST'])
 def deleteFAQ():
     if request.method == 'POST':
         id = request.form('id')
         faq = FAQ.query.filter_by(id = id).first()
         db.session.delete(faq)
+        db.session.commit() 
+        return redirect('http://localhost:3000/faqUpdate')
+
+@FAQ.route('/updateFAQ', methods=['GET', 'POST'])
+def updateFAQ():
+    if request.method == 'POST':
+        faq = FAQ.query.filter_by(id = id).first()
+        question = request.form('question')
+        answer = request.form('answer')
+        faq.question = question
+        faq.answer = answer
         db.session.commit() 
         return redirect('http://localhost:3000/faqUpdate')
