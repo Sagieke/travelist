42329d3a0d0fe30493c3919d61c83792374495d9
guykolbck <93705741+guykolbck@users.noreply.github.com>
2021-12-25 12:05:20 +0200

    Merge branch 'SAGIE' into GUY

---
+++ react-frontend/src/components/AdminPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Userlist/button";
+import Users from "./Userlist/users";
+
+
+export default function  AdminPage()  {
+   
+    return (
+    <div>
+        <Button/>
+          <Users/>
+         </div>
+    );
+}
