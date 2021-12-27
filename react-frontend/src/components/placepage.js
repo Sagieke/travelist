bba604976c34512b59166e54167a131b3acc96b3
zebigdt <dannytalor96@gmail.com>
2021-12-28 01:57:45 +0200

    update placepage

--- react-frontend/src/components/placepage.js
+++ react-frontend/src/components/placepage.js
@@ -1,10 +1,21 @@
 import EquipmentChecklist from "./EquipmentCheckList";
 import WeatherPage from "./weather";
-import React from "react";
+import React ,{ useState, useEffect }from "react";
 import Countdown from "./countdown";
 import {Container,Row, Col,Button,Modal,ListGroup,} from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 export default function  PlacePage(props)  {
+
+  const [placedata, setplacedata] = useState({});
+  
+  useEffect(() => {
+    fetch('http://localhost:5000/getPlaceInfo',{
+      credentials: "include"
+    })
+    .then(response => response.json())
+    .then(object => setplacedata(object))
+  },[]);
+
   return (
     <div className="auth-wrapper">
     <Row>
@@ -22,16 +33,16 @@ export default function  PlacePage(props)  {
       <div className="auth-inner-center">
       <h1>list of attractions</h1>
       <hr class="my-4"></hr>
-      <Countdown date="2-1-22"/>
+      <Countdown date='12-30-21'/>
       </div>
-
+      
       <div className="auth-inner-right">
       <Row>
       <EquipmentChecklist/>
       </Row>
       </div>
     </Row>
-    
     </div>
+    
   );
 }
