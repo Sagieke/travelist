a2ee0fe00ac502bfec3576e4b1e285e37b173227
zebigdt <dannytalor96@gmail.com>
2021-12-27 21:41:15 +0200

    register userpage blueprint

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -22,16 +22,18 @@ socketio = SocketIO(app) #sockets
 CORS(app,supports_credentials=True) #Cross-Origin Resource Sharing
 
 #blueprints initialization
-from endpoints.Chat import chat_blueprint
 from endpoints.Homepage import Homepage
+from endpoints.Userpage import Userpage
 from endpoints.ListOfListsPage import ListOfListsPage
 from endpoints.ListofPlacesPage import ListOfPlacesPage
+from endpoints.Chat import chat_blueprint
 from endpoints.Message import Message
 from endpoints.Userlist import Userlist
-app.register_blueprint(chat_blueprint)
 app.register_blueprint(Homepage)
+app.register_blueprint(Userpage)
 app.register_blueprint(ListOfListsPage)
 app.register_blueprint(ListOfPlacesPage)
+app.register_blueprint(chat_blueprint)
 app.register_blueprint(Message)
 app.register_blueprint(Userlist)
 
