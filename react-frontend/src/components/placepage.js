a9fd5a82cb8b61d63f94822c4232b5a74d0c752a
zebigdt <dannytalor96@gmail.com>
2021-12-27 23:54:38 +0200

    added equipmentchecklist to show

--- react-frontend/src/components/placepage.js
+++ react-frontend/src/components/placepage.js
@@ -1,3 +1,4 @@
+import EquipmentChecklist from "./EquipmentCheckList";
 import WeatherPage from "./weather";
 import React from "react";
 import {Container,Row, Col,Button,Modal,ListGroup,} from "react-bootstrap";
@@ -13,26 +14,23 @@ export default function  PlacePage(props)  {
         <div>
           <WeatherPage/>
         </div>
-        
       </Row>
       <br/>
-      <Row>
-       
-      </Row>
-      </div>
+    </div>
       <br />
       <div className="auth-inner-center">
       <h1>list of attractions</h1>
       <hr class="my-4"></hr>
       <Row>
-       
+       <div>
+        
+       </div>
       </Row>
       </div>
+
       <div className="auth-inner-right">
-      <h1>recommended attractions</h1>
-      <hr class="my-4"></hr>
       <Row>
-        
+      <EquipmentChecklist/>
       </Row>
       </div>
     </Row>
