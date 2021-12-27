dffa7df6f347fa1ced6a7358391e3c28feb7fb40
Sagieke <90636945+Sagieke@users.noreply.github.com>
2021-12-27 17:21:32 +0200

    Update UserList.js

--- react-frontend/src/components/UserList.js
+++ react-frontend/src/components/UserList.js
@@ -8,14 +8,23 @@ export default function UserList()  {
     const handleShow = () => setShow(true);  
     const [ShowUserList, setUserList] = useState([]);
 
-    useEffect(() => {
+     useEffect(() => {
         fetch('http://localhost:5000/getUserlist',{
             credentials: "include"
-          })
-            .then(response => response.json())
-            .then(object => console.log(object))
+         })
+          .then(response => response.json())
+           .then(object => setUserList(object))
         },[]);
-  
+    const ListUsr = (lui) => {
+
+      return (
+        <tr>
+        <td>{lui.id}</td>
+        <td>{lui.username}</td>
+        <td>{lui.usertype}</td>
+      </tr>
+             )   
+  };
 return (
     <>
   
@@ -26,22 +35,21 @@ return (
       <Modal.Title><h3> User list :</h3></Modal.Title>
     </Modal.Header>
     <Modal.Body>
-    <Table striped bordered hover size="sm"> 
-    <tbody style={{'height': '300px', 'overflow':'scroll', 'display': 'block'}}> 
+    <div style="overflow: scroll;"> 
+    <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>email</th>
+      <th>type</th> 
+    </tr>
+  </thead>
+  <tbody>
+ {ShowUserList.map(ListUsr)} 
 
-    <thead>
-<th>ID</th>
-<th>Emal adress</th>
-<th>Type</th>
-
-    </thead>
-    
-    
-    
-    
-    
-    </tbody>
-   </Table>
+  </tbody>
+</Table>
+</div>
    </Modal.Body>
     <Modal.Footer>
            User list:
