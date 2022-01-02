e3a3a2d5fbe4734af7e25ae05a66eef4abfa215c
zebigdt <dannytalor96@gmail.com>
2022-01-03 00:10:40 +0200

    removed username checks

--- react-frontend/src/components/NavBarItems/login.component.js
+++ react-frontend/src/components/NavBarItems/login.component.js
@@ -18,42 +18,6 @@ export default function Login() {
     const handleShow = () => setShow(true);
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
-    const [passwordError, setpasswordError] = useState("");
-    const [emailError, setemailError] = useState("");
-
-    const handleValidation = (event) => {
-        let formIsValid = true;
-
-        if (!userName.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
-            formIsValid = false;
-            setemailError("Email Not Valid");
-            return false;
-        } else {
-            setemailError("");
-            formIsValid = true;
-        }
-
-        if (!password.match(/^[a-zA-Z0-9]{8,22}$/)) {
-            formIsValid = false;
-            setpasswordError(
-                "Only Letters and length must best min 8 Chracters and Max 22 Chracters"
-            );
-            return false;
-        } else {
-            setpasswordError("");
-            formIsValid = true;
-        }
-        if (formIsValid == true) { handleClose() }
-        return formIsValid;
-    };
-
-    const loginSubmit = (e) => {
-        e.preventDefault();
-        handleValidation();
-
-    };
-
-
 
     return (
         <>
@@ -75,9 +39,6 @@ export default function Login() {
                   placeholder="Enter email"
                   onChange={event => setUserName(event.target.value)} 
                 />
-                <small id="emailHelp" className="text-danger form-text">
-                        {emailError}
-                      </small>
               </div>
               <div className="form-group">
                 <label>Password</label>
@@ -88,9 +49,6 @@ export default function Login() {
                   placeholder="Enter password"
                   onChange={event => setPassword(event.target.value)}
                 />
-                <small id="passworderror" className="text-danger form-text">
-                        {passwordError}
-                      </small>
               </div>
       
               <div className="form-group">
