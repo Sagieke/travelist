a740c1befa25dc1ad75ed56b2bf2060863bfd102
guykolbck <93705741+guykolbck@users.noreply.github.com>
2022-01-02 16:24:50 +0200

    Moved to new folders

---
+++ react-frontend/src/components/Pages/MainPage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import  Login from "../NavBarItems/login.component";
+import SignUp from "../NavBarItems/signup.component";
+import UserHelp from "../UserComponants/UserHelp";
+
+
+export default function  MainPage()  {
+   
+    return (
+    
+
+      
+    <div className="container">
+    <div className="App-login">
+          <Login/>  
+          <SignUp/>
+          <UserHelp/>
+         </div>
+         
+      </div> 
+    )
+    }
+
+    
+
+
+
+
+
