31c1c60bb4c51bc8e2020360cfe5369b07e2d1fe
guykolbck <93705741+guykolbck@users.noreply.github.com>
2022-01-02 16:26:49 +0200

    New image slider for front page

---
+++ react-frontend/src/components/Pages/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+    {
+      image:
+        'https://i.postimg.cc/Jnj3GMqT/About-us-joke-1.png'
+    },
+    {
+      image:
+        'https://i.postimg.cc/zXhxJXrR/image.png'
+    },
+    {
+      image:
+        'https://i.postimg.cc/SNtmpdmK/1.png'
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1578592391689-0e3d1a1b52b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1627289496743-8a9a08bb228a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1440186347098-386b7459ad6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    ];
