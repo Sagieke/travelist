da8de1add6120d10575fc30f68b03bbde0e4919e
guykolbck <93705741+guykolbck@users.noreply.github.com>
2022-01-04 17:57:47 +0200

    Admin updates ( suggestions,messages,userlist)

---
+++ react-frontend/src/components/AdminComponants/AddmassegeAdmin.js
@@ -0,0 +1,71 @@
+import React,{ useState } from "react";
+import {Button,Modal,Dropdown,DropdownButton} from "react-bootstrap";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import { AiFillEdit } from 'react-icons/ai';
+
+const ButtonStyle1={
+  mergin:"10px",
+  borderColor:"black",
+  width: '150px',
+  height: '30px',
+  backgroundColor:"grey",
+  color:"black",
+}
+
+
+
+export default function  AddMessageAdmin(props)  {
+  const [show, setShow] = useState(false);
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);  
+const [answer, setTitle] = useState('');
+const [question, setDescription] = useState('');
+const [id, setid] = useState('');
+
+return (
+  <>
+
+  <Button style={ButtonStyle1} variant="primary" onClick={handleShow}><AiFillEdit/>Add Message</Button>
+  <Modal show={show} onHide={handleClose}>
+  <form action='http://localhost:5000/messageSenderFromAdminToAll' method='post' >
+  <Modal.Header closeButton>
+    <Modal.Title><h3> Add Massege</h3></Modal.Title>
+  </Modal.Header>
+  <Modal.Body>
+
+
+  <div className="form-group">
+                <label>Title:</label>
+                <input
+                  name="title"
+                  type="title"
+                  className="form-control"
+                  placeholder="Enter title"
+                  onChange={event => setTitle(event.target.value)} 
+                  required
+                />
+              
+              </div>
+              <div className="form-group">
+                <label>Description</label>
+                <input
+                  name="description"
+                  type="description"
+                  className="form-control"
+                  placeholder="Enter description"
+                  onChange={event => setDescription(event.target.value)}
+                />
+             
+              </div>
+    
+ </Modal.Body>
+  <Modal.Footer>
+  <button type="submit" className="btn btn-primary btn-block" >
+          register
+        </button>
+  </Modal.Footer>
+  </form>
+</Modal>
+  </> 
+ ); 
+}
---
+++ react-frontend/src/components/AdminComponants/AdminMasseges.js
@@ -0,0 +1,88 @@
+import React , { useState, useEffect, Componant}from "react";
+import {Container,Row, Col,Button,Modal,ListGroup,Table,Dropdown,DropdownButton,} from "react-bootstrap";
+import { BiEnvelope } from 'react-icons/bi';
+import AddMessageAdmin from "./AddmassegeAdmin";
+
+
+
+const ButtonStyle2={
+  color:"black",
+  height:"50px",
+  borderColor:"black",
+  backgroundColor:"Lightgreen",
+  margin:"10px",
+  
+}
+ 
+
+
+const DeleteFaqStyleButton={
+  borderColor:"black",
+  width: '150px',
+  height: '30px',
+  backgroundColor:"red",
+  color:"black",
+}
+
+
+export default function AdminMessage()  {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);  
+    const [ShowMsglist, setMsgList] = useState([]);
+    const[UserId,setUserId]=useState('');
+
+
+     useEffect(() => {
+        fetch('http://localhost:5000/getMessageAdmin',{
+            credentials: "include"
+         })
+          .then(response => response.json())
+           .then(object => setMsgList(object))
+        },[]);
+    const ListMsg = (msg) => {
+
+      return (
+        <tr>
+        <td>{msg.id}</td>
+        <td>{msg.title}</td>
+        <td>{msg.description}</td>        
+     </tr>
+             )   
+  };
+ return (
+    <>
+
+
+<form ><Button style={ButtonStyle2}variant="primary" onClick={handleShow}><BiEnvelope/>View masseges</Button></form>
+  <Modal size="lg" show={show} onHide={handleClose}>
+  <form action='http://localhost:5000//getAllMessageTech' method='post' name="id" >
+  <Modal.Header closeButton>
+    <Modal.Title><h3> Messege list:</h3></Modal.Title>
+  </Modal.Header>
+  <Modal.Body>
+    <div style={{'height': '300px', 'overflow':'scroll', 'display': 'block',}}>
+    <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      
+      <th> id</th>
+       <th>title</th>
+       <th>description</th>
+    
+    </tr>
+  </thead>
+  <tbody>
+ {ShowMsglist.map(ListMsg)} 
+  </tbody>
+</Table>
+</div>
+   </Modal.Body>
+    <Modal.Footer> 
+            <AddMessageAdmin/>
+    </Modal.Footer>
+    </form>
+  </Modal>
+    </> 
+   ); 
+ }
