8f697a8cb0343e70b81df1255be5578754f19d31
zebigdt <dannytalor96@gmail.com>
2022-01-05 16:39:16 +0200

    updated userlist

--- flask-backend/endpoints/Userlist.py
+++ flask-backend/endpoints/Userlist.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, redirect
 from app import db
-from models import User
+from models import User,NumberofUsers
 
 Userlist = Blueprint('Userlist',__name__)
 
@@ -18,6 +18,8 @@ def changePermissionTech():
         if role == 'TECH' :
             user = User.query.filter_by(id = user_id).first()
             user.usertype = 'TechSupport'
+            user.answers = 0
+            user.rating = 0
             db.session.commit()
             return redirect('http://localhost:3000/techsupport')
         if role == 'TRAVELER':
@@ -35,6 +37,8 @@ def changePermissionAdmin():
         if role == 'TECH' :
             user = User.query.filter_by(id = user_id).first()
             user.usertype = 'TechSupport'
+            user.answers = 0
+            user.rating = 0
             db.session.commit()
             return redirect('http://localhost:3000/adminpage')
         if role == 'ADMIN':
@@ -54,7 +58,8 @@ def deleteUser():
     if request.method == 'POST':
         user_id = request.form['id']
         user = User.query.filter_by(id = user_id).first()
+        num = NumberofUsers.query.filter_by(id = 1).first()
+        num.number_of_users -= 1
         db.session.delete(user)
         db.session.commit()
         return redirect('http://localhost:3000/adminpage')
-
