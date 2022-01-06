19c2cc33bc7138ad5d1ab4e7e8d8a4b57c7c34bc
Sagieke <90636945+Sagieke@users.noreply.github.com>
2022-01-06 22:18:14 +0200

    requirement number 203

--- react-frontend/src/components/UserComponants/PageNotFound.js
+++ react-frontend/src/components/UserComponants/PageNotFound.js
@@ -1,9 +1,5 @@
 import React,{ useState } from "react";
-import {Button,Modal,Accordion,ListGroup, ListGroupItem} from "react-bootstrap";
-
-
-
-
+//requirement number 203 
 export default function PageNotFound()  {
     
     return(
